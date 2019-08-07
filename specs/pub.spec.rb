6ce5b6510bf7a3ecf82e595f5b1b2ffb9e391061
Cameron Pellett <bubionbreakfast@gmail.com>
2019-08-07 11:36:12 +0100

    can get pub name

--- specs/pub.spec.rb
+++ specs/pub.spec.rb
@@ -6,7 +6,15 @@ require_relative('../drink')
 
 class TestPub < MiniTest::Test
 
+def setup()
+  @drink1 = Drink.new("Beer", 2)
+  @drink2 = Drink.new("Wine", 3)
+  @drink3 = Drink.new("Spirits", 4)
+  @pub = Pub.new("Bobs", 10, [@drink1, @drink2, @drink3])
+end
 
-
+def test_get_pub_name()
+  assert_equal("Bobs", @pub.name())
+end
 
 end
